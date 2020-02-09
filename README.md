e84bddf223391d9a84ffa7d24b001671c06f6b10
John Nguyen <johntnguyen9@gmail.com>
2020-02-08 20:31:57 -0600

    Update readme and add flask files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # connecepts
 connect concepts
+
+install virtual env
+
+$ python3 -m venv venv
+
+activate virtual environment
+
+$ . venv/bin/activate
+
+install dependencies
+
+$ pip install -r requirements.txt
+
+test locally (not on production)
+
+$ flask run --debugger
+
+install React dependencies
+
+$ cd my-app
+
+$ npm install
+
+Build production bundle
+
+$ npm run build
+
+
+
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask # import main Flask class and request object
+import requests
+app = Flask(__name__) # flask app
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/query-example')
+def query_example():
+    r = requests.get('http://api.conceptnet.io/c/en/example').text
+    return r
+    # keys = json.loads(r).keys() 
+    # return json.dumps(keys)
+    
+
+if __name__ == '__main__':
+  app.run(debug=True, port=5000) #run app in debug mode on port 5000
+  query_example()
